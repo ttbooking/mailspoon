9a72f0ca89d938c38e6fd7411f81b709a9b093a3
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2026-03-15 19:33:45 +0200

    Bug fixes

--- app/Listeners/NotifyNewMessage.php
+++ app/Listeners/NotifyNewMessage.php
@@ -2,7 +2,7 @@
 
 namespace App\Listeners;
 
-use Exception;
+use Illuminate\Container\Attributes\Config;
 use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Support\Facades\Http;
 use Random\RandomException;
@@ -10,26 +10,24 @@ use Webklex\IMAP\Events\MessageNewEvent;
 
 class NotifyNewMessage
 {
+    public function __construct(
+        #[Config('spoon.endpoint')] protected string $endpoint,
+        #[Config('spoon.key')] protected string $key,
+    ) {}
+
     /**
      * Handle the event.
      *
      * @throws ConnectionException
      * @throws RandomException
-     * @throws Exception
      */
     public function handle(MessageNewEvent $event): void
     {
-        $config = config('spoon');
-
-        if (! isset($config['endpoint'], $config['key'])) {
-            throw new Exception('Mailspoon service is not configured.');
-        }
-
-        Http::asForm()->post($config['endpoint'], [
-            'body-mime' => $event->message->getRawBody(),
-            'timestamp' => $timestamp = $event->message->date,
+        Http::asForm()->post($this->endpoint, [
+            'body-mime' => $event->message->getHeader()->raw."\r\n".$event->message->getRawBody(),
+            'timestamp' => $timestamp = $event->message->date->toDate()->getTimestamp(),
             'token' => $token = bin2hex(random_bytes(25)),
-            'signature' => hash_hmac('sha256', $timestamp.$token, $config['key']),
+            'signature' => hash_hmac('sha256', $timestamp.$token, $this->key),
         ]);
     }
 }
