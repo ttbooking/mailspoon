fb2b43f79992fe8d73386ef4be868381aed71b92
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2026-04-02 12:41:42 +0200

    Switch to ImapEngine library

--- app/Listeners/NotifyNewMessage.php
+++ app/Listeners/NotifyNewMessage.php
@@ -2,11 +2,11 @@
 
 namespace App\Listeners;
 
+use DirectoryTree\ImapEngine\Laravel\Events\MessageReceived;
 use Illuminate\Container\Attributes\Config;
 use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Support\Facades\Http;
 use Random\RandomException;
-use Webklex\IMAP\Events\MessageNewEvent;
 
 class NotifyNewMessage
 {
@@ -21,11 +21,11 @@ class NotifyNewMessage
      * @throws ConnectionException
      * @throws RandomException
      */
-    public function handle(MessageNewEvent $event): void
+    public function handle(MessageReceived $event): void
     {
         Http::asForm()->post($this->endpoint, [
-            'body-mime' => $event->message->getHeader()->raw."\r\n".$event->message->getRawBody(),
-            'timestamp' => $timestamp = $event->message->date->toDate()->getTimestamp(),
+            'body-mime' => (string) $event->message,
+            'timestamp' => $timestamp = $event->message->date()?->getTimestamp() ?? time(),
             'token' => $token = bin2hex(random_bytes(25)),
             'signature' => hash_hmac('sha256', $timestamp.$token, $this->key),
         ]);
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,13 +1,7 @@
 <?php
 
-use App\Console\Commands\ImapIdleCommand;
-use App\Console\Commands\ImapPullCommand;
 use Illuminate\Foundation\Application;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withCommands([
-        ImapIdleCommand::class,
-        ImapPullCommand::class,
-    ])
     ->withExceptions()
     ->create();
